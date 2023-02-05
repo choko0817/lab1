139f2f0bc7acfb84822ef663a2e40929217b648b
choko <choko@DESKTOP-VH0CSFF>
2023-02-05 02:39:59 -0800

    add code

--- Lab1/src/Stack.java
+++ Lab1/src/Stack.java
@@ -1,9 +1,67 @@
+import System;
+import System.Collections.Generic;
+import System.Linq;
+import System.Text;
 
-public class Stack {
+namespace Example
+{
+    class Stack
+    {
+        private int count, size;
+        private int[] array;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        public Stack(int n)
+        {
+            array = new int[n];
+            size = n;
+            count = 0;
+        }
 
-	}
+        public bool isFull()
+        {
+            return count == size;
+        }
 
+        public bool isEmpty()
+        {
+            return count == 0;
+        }
+
+        public void push(int value)
+        {
+            if (!isFull())
+            {
+                array[count++] = value; 
+            }
+        }
+
+        public int pop()
+        {
+            if (!isEmpty())
+                return array[--count];
+            else
+                //return -1;
+            //return default(int);
+            throw new InvalidOperationException("Stack empty!");
+        }
+
+        public int top()
+        {
+            if (!isEmpty())
+                return array[count-1];
+            else
+                //return -1;
+                //return default(int);
+                throw new InvalidOperationException("Stack empty!");
+        }
+
+        public string toString()
+        {
+            string s = "";
+            for (int i = 0; i < count; i++)
+                s = s + array[i].ToString() + " ";
+
+            return s;
+        }
+    }
 }
